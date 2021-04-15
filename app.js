c72a119da7b570103af2271b763a8ef566b265a0
shakib1111 <skshakib0000@gmail.com>
2021-04-16 01:03:26 +0530

    txt added

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ form.addEventListener('submit', e =>{
     e.preventDefault();
 
     let score=0;
-    const userAnswers=[form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value,form.q6.value,form.q7.value,form.q8.value,form.q9.value,form.q10value]
+    const userAnswers=[form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value,form.q6.value,form.q7.value,form.q8.value,form.q9.value,form.q10.value];
 
 
     //check answers
     userAnswers.forEach((answer,index) =>{
         if(answer === correctAnswers[index])
         {
-            score += 20;
+            score += 10;
         }
 
+        
     });
 
+    console.log(score)
    
 });
